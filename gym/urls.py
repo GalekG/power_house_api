f0acebc2ceb67166e1e60fc2c2210266786ef529
Kevin Alexander Checa Díaz <alebean99@gmail.com>
2023-10-26 14:02:13 -0500

    added machines crud

--- gym/urls.py
+++ gym/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from gym.views.users_views import user_list, create_user, user_detail, update_user, delete_user
+from gym.views import users_views, machines_views
+
 
 urlpatterns = [
-    path('users/', user_list, name='users.list'),
-    path('users/create/', create_user, name='users.create'),
-    path('users/<int:user_id>/', user_detail, name='users.getById'),
-    path('users/<int:user_id>/update/', update_user, name='users.update'),
-    path('users/<int:user_id>/delete/', delete_user, name='users.delete'),
+    path('users/', users_views.user_list, name='users.list'),
+    path('users/create/', users_views.create_user, name='users.create'),
+    path('users/<int:user_id>/', users_views.user_detail, name='users.getById'),
+    path('users/<int:user_id>/update/', users_views.update_user, name='users.update'),
+    path('users/<int:user_id>/delete/', users_views.delete_user, name='users.delete'),
+    path('machines/', machines_views.machine_list, name='machines.list'),
+    path('machines/create/', machines_views.create_machine, name='machines.create'),
+    path('machines/<int:machine_id>/', machines_views.machine_detail, name='machines.getById'),
+    path('machines/<int:machine_id>/update/', machines_views.update_machine, name='machines.update'),
+    path('machines/<int:machine_id>/delete/', machines_views.delete_machine, name='machines.delete'),
+
 ]
 
